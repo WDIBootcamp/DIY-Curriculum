eb7addd9c756a1807b95aaca25494a8a09aadd1e
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-06-15 14:14:27 -0300

    landing page controller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,7 @@ class SitesController < ApplicationController
   include CoursesHelper
 
   def index
-  @contents = Content.limit(3)
+  @contents = Content.find([13, 10, 5])
   @index_page = true;
   end
 end
